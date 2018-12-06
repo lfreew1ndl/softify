684c2da46f52f42b3bf8efaa53bd970d992d9767
Salim <salimabdul.it@gmail.com>
2018-12-06 17:15:01 +0200

    Initial commit

---
+++ src/main/java/com/project/softify/SoftifyApplication.java
@@ -0,0 +1,12 @@
+package com.project.softify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftifyApplication.class, args);
+    }
+}
