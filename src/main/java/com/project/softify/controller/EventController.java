43e20fb994fcaf8d15ef1af48c6a0e36c30cb33a
Salim <salimabdul.it@gmail.com>
2018-12-08 11:16:44 +0200

    added event service

--- src/main/java/com/project/softify/controller/EventController.java
+++ src/main/java/com/project/softify/controller/EventController.java
@@ -1,6 +1,8 @@
 package com.project.softify.controller;
 
 import com.project.softify.model.Event;
+import com.project.softify.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventController {
 
+    @Autowired
+    EventService eventService;
 
     @RequestMapping("/addEvent")
     public void addEvent(Event event){
--- src/main/java/com/project/softify/service/EventServiceImpl.java
+++ src/main/java/com/project/softify/service/EventServiceImpl.java
@@ -3,10 +3,12 @@ package com.project.softify.service;
 import com.project.softify.model.Event;
 import com.project.softify.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EventServiceImpl implements EventService {
 
     @Autowired
